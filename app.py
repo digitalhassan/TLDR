352e247ee60618f4ba135b251ac8cbd717cb1186
digitalhassan <102837263+digitalhassan@users.noreply.github.com>
2023-03-25 18:03:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,23 @@
 import os
-from flask import Flask, request
-from telegram import Bot, Update
-from telegram.ext import CommandHandler, MessageHandler, filters, dispatcher
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
-# This is a Telegram Bot Test
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Hi!")
 
-TOKEN = os.getenv('TELEGRAM_TOKEN')
-APP_URL = os.getenv('APP_URL')
-
-app = Flask(__name__)
-bot = Bot(TOKEN)
-dp = Dispatcher(bot, None)
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello this is the first bot Hassan has made!")
-
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
-
-@app.route(f'/{TOKEN}', methods=['POST'])
-def webhook_handler():
-    if request.method == "POST":
-        update = Update.de_json(request.get_json(force=True), bot)
-        dp.process_update(update)
-    return "ok"
+def echo(update: Update, context: CallbackContext):
+    update.message.reply_text(update.message.text)
 
 def main():
+    api_key = os.environ.get("TELEGRAM_API_KEY")
+    updater = Updater(api_key, use_context=True)
+    dp = updater.dispatcher
+
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(filters.text & ~filters.command, echo))
-    dp.add_error_handler(error)
+    dp.add_handler(MessageHandler(filters.Text(), echo))
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    updater.start_polling()
+    updater.idle()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
