204ddaf1a7b74241e0c492a36c16aea551d31ea2
digitalhassan <102837263+digitalhassan@users.noreply.github.com>
2023-03-25 17:54:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram.ext import CommandHandler, MessageHandler, filters, Dispatcher
 
 # This is a Telegram Bot Test
 
@@ -30,7 +30,7 @@ def webhook_handler():
 
 def main():
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(filters.text & ~filters.command, echo))
     dp.add_error_handler(error)
 
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
